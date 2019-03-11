f2404137c9af4689e3010cbd02e41ae320de2215
Riccardo <riki-95@hotmail.it>
2019-03-11 16:15:35 +0100

    Perturbation
    Co-Authored-By: Davide Ponzini <davide.ponzini95@gmail.com>
    Co-Authored-By: Daniele <dany2305@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
 
 
 def create_random_graph(n, p):
@@ -28,10 +29,10 @@ def create_ex_graph():
     return g
 
 
-def draw_graph(g):
+def draw_graph(g, name):
     nx.draw_networkx(g, pos=nx.spring_layout(g), node_size=50, font_size=10, font_color='b', arrowsize=3)
     plt.draw()
-    plt.savefig('img.png', dpi=500)
+    plt.savefig('{}.png'.format(name), dpi=500)
     plt.close()
 
 
@@ -48,7 +49,7 @@ def compute_degree(g):
         if (degree_node < min_degree):
             min_degree = degree_node
 
-    return total_degree, max_degree, min_degree
+    return total_degree/g.number_of_nodes(), max_degree, min_degree
 
 
 def print_measurements(g):
@@ -57,8 +58,8 @@ def print_measurements(g):
     print('Number of edges:', g.number_of_edges())
     print('Number of self-loops:', g.number_of_selfloops())
     
-    total_degree, max_degree, min_degree = compute_degree(g)
-    print('\nTotal Degree: ', total_degree/number_of_nodes)
+    avg_degree, max_degree, min_degree = compute_degree(g)
+    print('\nTotal Degree: ', avg_degree)
     print('Max Degree: ', max_degree)
     print('Min Degree: ', min_degree)
 
@@ -98,21 +99,64 @@ def deanonymize(g, i):
     h = hi(g, i)
     eq = eq_class(h)
 
-    print('[h{}] {} classes'.format(i, len(eq)))
-    print('[h{}] {:.0%} deanonymization'.format(i, len(eq) / len(g)))
+    deanonymized_nodes = {v[0] for k,v in eq.items() if len(v) == 1}
+
+    print('[h{}] {} nodes'.format(i, len(g)))
+    print('[h{}] {} equivalence classes ({:.0%})'.format(i, len(eq), len(eq) / len(g)))
+
+    print('[h{}] {:.0%} deanonymization'.format(i, len(deanonymized_nodes) / len(g)))
+    #print(deanonymized_nodes)
+    
     print()
 
+
+# remove p% of edges from each node
+# add the same number of edge to DIFFERENT nodes
+def perturbation(graph, p):
+    g = graph.copy()
+    edges_to_remove = int(len(g.edges()) * p)
+    print('Will remove {} edges'.format(edges_to_remove))
+    print('Before perturbation: ', g.edges())
+    
+    removed_edges = []
+    for i in range(edges_to_remove):
+        random_edge = random.choice(list(g.edges()))
+        g.remove_edges_from(random_edge)
+        removed_edges.append(random_edge)
+    print('Removed: ', removed_edges)
+
+    print('\nNew graph: ', g.edges())
+    while(edges_to_remove > 0):
+        first_node = random.choice(list(g.nodes()))
+        second_node = random.choice(list(g.nodes()))
+        if(second_node == first_node):
+            continue
+        if g.has_edge(first_node, second_node) or (first_node, second_node) in removed_edges or (second_node, first_node) in removed_edges:
+            continue
+        else:
+            g.add_edge(first_node, second_node)
+            #print('Add: ', first_node, second_node)
+            edges_to_remove -= 1
+    
+    print('Final graph', g.edges())
+    return g
+
+
 if __name__ == '__main__':
-    g = create_random_graph(500, 0.01)
+    #g = create_random_graph(500, 0.01)
     #g = create_scale_free_graph(50)
-    #g = create_ex_graph()
+    g = create_ex_graph()
 
-    #print_measurements(g)
+    print_measurements(g)
 
-    for i in range(1, 5):
-        deanonymize(g, i)
+    #for i in range(1, 5):
+    #    deanonymize(g, i)
     
     #for k,v in eq.items():
     #    print('{}: {}'.format(k,v))
 
-    #draw_graph(g)
+    #draw_graph(g, 'before_perturbation')
+
+    pert_graph = perturbation(g, 0.5)
+    print_measurements(pert_graph)
+    #draw_graph(pert_graph, 'after_perturbation')
